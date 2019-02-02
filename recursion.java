cdbf5cb9914e01cc1d1e379a0a0780507554defb
Alvi Aziz <aaziz00@stuy.edu>
2019-02-01 22:06:26 -0500

    one test for makeAllSums

---
+++ recursion.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class recursion{
+  public static void main(String[] args){
+    System.out.println("TESTING makeAllSums\n------------------------\n");
+    System.out.println("//Should print [0, 3, 2, 5, 1, 4, 3, 6]");
+    System.out.println(makeAllSums(3));
+  }
+
+
+  public static ArrayList<Integer> makeAllSums(int n){
+    ArrayList<Integer> nums = new ArrayList<Integer>();
+    addSums(nums,n,1,0);
+    return nums;
+  }
+  public static void addSums(ArrayList<Integer> mod, int n, int index, int sum){
+    if (index <= n){
+      addSums(mod,n,index+1,sum+index);
+      addSums(mod,n,index+1,sum);
+    }
+    else{
+      mod.add(sum);
+    }
+  }
+
+
+}
